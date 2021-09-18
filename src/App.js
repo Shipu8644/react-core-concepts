0e73f80a0850c3c02e4e5f58c3dbd811ae9991c1
Shipu8644 <shipu.monjurur@gmail.com>
2021-09-18 20:20:11 +0600

    done

--- src/App.js
+++ src/App.js
@@ -2,25 +2,67 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const nayoks = ['Rubel', 'Bapparaz', 'Koober', 'Bappi', 'Shuvo', 'Omar Sani'];
+  const cinemas = [
+    { nayok: 'Koober', nayika: 'kopila' },
+    { nayok: 'Rubel', nayika: 'Moushumi' },
+    { nayok: 'Razzak', nayika: 'Shabana' },
+    { nayok: 'Jashim', nayika: 'Suchorita' },
+
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <h3>Hey React</h3>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {nayoks.map(nayok => <li>{nayok}</li>)}
+
+      </ul>
+      {
+        cinemas.map(cinema => <Cinema nayok={cinema.nayok} nayika={cinema.nayika}  ></Cinema>)
+      }
     </div>
   );
 }
+
+function Person(props) {
+  // console.log(props);
+  const person = {
+    backgroundColor: "skyblue",
+    border: "3px solid lightsalmon",
+    margin: "20px",
+    borderRadius: "10px"
+  }
+  return (
+    <div style={person}>
+      <h1>Name: {props.name}</h1>
+      <h3>Profession: {props.profession}</h3>
+    </div>
+  );
+}
+function Friend(props) {
+  return (
+    <div className='person'>
+      <h3>Phone: {props.phone}</h3>
+      <h5>Adress: {props.address} </h5>
+    </div>
+  );
+}
+function Cinema(props) {
+  return (
+    <div className="person">
+      <h2>Nayok: {props.nayok}</h2>
+      <h4>Nayika: {props.nayika}</h4>
+    </div>
+  );
+}
+
+{/* <p id="title">Title</p>
+      <Friend phone="01700" address="Noakhali"></Friend>
+      <Friend phone="01899" address="Rongpur"></Friend>
+      <Friend phone="01655" address="Jessore"></Friend>
+      <Friend phone="01900" address="Dhaka"></Friend> */}
+
+{/* <Person name='Shipu' profession='Computer Engineer'></Person>
+      <Person name='Kona' profession='Accountant Manager'></Person>
+      <Person name='Jahnaker Mahbub' profession='Boss'></Person> */}
 
 export default App;
